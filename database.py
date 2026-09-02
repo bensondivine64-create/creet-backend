e5bbc5637ae21e3b4b7a89a72a07d231d33e2222
bensondivine64 <bensondivine64@gmail.com>
2026-09-02 10:18:48 +0100

    Fix MySQL SSL connection args

--- database.py
+++ database.py
@@ -8,7 +8,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./creet.db")
 
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+elif DATABASE_URL.startswith("mysql"):
+    connect_args = {"ssl": {}}
+else:
+    connect_args = {}
+
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
