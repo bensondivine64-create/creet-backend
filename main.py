b01b29688db3d79d06b33d770692bade1b26c271
bensondivine64 <bensondivine64@gmail.com>
2026-09-02 07:55:34 +0100

    Configure Flask for Render

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=False)
